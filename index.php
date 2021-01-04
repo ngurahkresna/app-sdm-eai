fc8fb7196ae366a448679cb43bde570cabe3f9b3
Ngurah Kresna <ngurahkresna99@gmail.com>
2021-01-04 15:46:38 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>Onboard</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #008B8B;">
+    <a class="navbar-brand" href="#" style="color: white;"><b>PENABIL</b></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-left: 1000px;">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active" style="float: right;">
+                <a class="nav-link" href="login.html"style="color: white;">Dashboard<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"style="color: white;">Contact</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="content" style="background-color: white;"></div>
+<div class="deskripsi" style="float: left; width: 50%; margin-left: 100px; margin-top: 100px;">
+    <h1>DIVISI SDM</h1>
+    <p>SDM merupakan suatu cara yang dibuat untuk mengatur sumber daya (tenaga kerja) yang ada di dalam perusahaan. Selain itu SDM sendiri memiliki fungsi untuk mengorganisasi, memotivasi, dan memberikan penyuluhan agar sumber daya manusia dalam perusahaan dapat berfungsi secara maksimal.</p>
+    <form action="login.html">
+        <button class="btn btn-primary btn-sm" style="background-color: #008B8B; width: 100px; border-color: #008B8B; border-radius: 20px; color: white;"> Login </button><br><br>
+</div>
+<div class="content" style="background-color: white;"></div>
+<div class="deskripsi" style="float: right; width: 50%; margin-right: 100px; margin-top: 100px;">
+    <h1>EMPLOYEE</h1>
+    <p>Fitur ini digunakan untuk menampilkan seluruh data pegawai. Return yang dihasilkan yaitu seluruh data pegawai pada divisi SDM</p>
+</div>
+<div class="content" style="background-color: white;"></div>
+<div class="deskripsi" style="float: left; width: 50%; margin-left: 100px; margin-top: 100px;">
+    <h1>ABSENCE</h1>
+    <p>Fitur ini digunakan untuk menyediakan informasi daftar absensi harian karyawan yang nantinya akan di generate untuk menghitung payroll.</p>
+</div>
+<div class="content" style="background-color: white;"></div>
+<div class="deskripsi" style="float: right; width: 50%; margin-right: 100px; margin-top: 100px;">
+    <h1>PAYROLL</h1>
+    <p>Fitur Penggajian merupakan fitur yang mengatur tentang masalah penggajian oleh pegawai yang nantinya akan tersinkronisasi dengan data presensi. Fitur ini digunakan untuk menampilkan seluruh daftar gaji pegawai pada divisi SDM.</p>
+</div>
+<div class="content" style="background-color: white;"></div>
+<div class="deskripsi" style="float: left; width: 50%; margin-left: 100px; margin-top: 100px;">
+    <h1>CATEGORY</h1>
+    <p>Fitur ini menampilkan daftar kategori golongan yang berlaku di perusahaan untuk mentukan besar sallary yang akan didapatkan.</p>
+</div>
+<div class="content" style="background-color: white;"></div>
+<div class="deskripsi" style="float: right; width: 50%; margin-right: 100px; margin-top: 100px;">
+    <h1>REIMBURSEMENT</h1>
+    <p>Fitur ini digunakan untuk menampilkan data claim reimbursement karyawan kepada perusahaan.</p>
+</div>
+<div class="footer" style="background-color: #008B8B; height: 200px; margin-top: 1600px;">
+    <div class="kontak" style="width: 300px; height: 100px; line-height: 10px; padding-top: 40px; padding-left: 40px; color: white; font-size: 12px;">
+        <p>Manufaktur Mobil SDM, Jl. Buah Batu no. 97</p>
+        <p>phone: 022 123456</p>
+        <p>fax: 022 123456</p>
+        <p>mail@test.com</p>
+    </div>
+    <div class="socmed">
+        <!-- ntar -->
+    </div>
+</div>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>
